53aceb40e687c932ff1f27802ebb336f7617dd80
Original-Tea <levshevchenko2800@gmail.com>
2026-08-06 09:09:42 +0300

    first commit

--- DashboardProjectAPI/obj/Debug/net10.0/DashboardProjectAPI.AssemblyInfo.cs
+++ DashboardProjectAPI/obj/Debug/net10.0/DashboardProjectAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676935e45c7b2c21cc5ae0898815b9c7072e00b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
